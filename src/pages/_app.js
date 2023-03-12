2ae29c4306f08581db25e8d25c03c67eff3d4f09
Richard passos <richard.ac.passos12@gmail.com>
2023-03-12 16:44:46 -0300

    switching fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,10 @@
 /* Style */
+import { Roboto } from "next/font/google";
 import ResetStyle from "@/styles/reset";
 import GlobalStyle from "@/styles/global";
 import { Layout } from "@/components";
+
+const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,9 +12,11 @@ export default function App({ Component, pageProps }) {
       <ResetStyle />
       <GlobalStyle />
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <main className={roboto.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
     </>
   );
 }
